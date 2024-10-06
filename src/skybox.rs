819bce782d58b0f71f08d19d2aa1683cd9764a6c
Cedric Martens <cedricmartens98@gmail.com>
2024-10-06 13:20:36 -0400

    Skybox

--- src/skybox.rs
+++ src/skybox.rs
@@ -2,11 +2,12 @@ use bevy::asset::Handle;
 use bevy::prelude::{Image, Resource};
 use bevy::render::texture::CompressedImageFormats;
 
-const CUBEMAPS: &[(&str, CompressedImageFormats)] = &[("skybox.png", CompressedImageFormats::NONE)];
+pub const CUBEMAPS: &[(&str, CompressedImageFormats)] =
+    &[("skybox.png", CompressedImageFormats::NONE)];
 
 #[derive(Resource)]
-struct Cubemap {
-    is_loaded: bool,
-    index: usize,
-    image_handle: Handle<Image>,
+pub struct Cubemap {
+    pub(crate) is_loaded: bool,
+    pub(crate) index: usize,
+    pub(crate) image_handle: Handle<Image>,
 }
